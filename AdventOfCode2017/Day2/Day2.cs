52d3d2280fd07e0ac7c594d3eeab79d7b92c7a7e
Vimefall <anders.vimefall@gmail.com>
2017-12-05 23:26:45 +0100

    Day1Part2, Day2Part2

--- AdventOfCode2017/Day2/Day2.cs
+++ AdventOfCode2017/Day2/Day2.cs
@@ -23,16 +23,78 @@ namespace AdventOfCode2017.Day2
 
             return result;
         }
+
+        public static int SummeraJamntDelbaraTal(string[] indata)
+        {
+            int result = 0;
+
+            foreach (var row in indata)
+            {
+                var numbers = row.Split((char[])null, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+                bool rowComplete = false;
+
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    for (int j = 0; j < numbers.Count; j++)
+                    {
+                        if (i == j)
+                        {
+                            continue;
+                        }
+                        if (numbers[i] % numbers[j] == 0)
+                        {
+                            result += numbers[i] / numbers[j];
+                            rowComplete = true;
+                            break;
+                        }
+
+                    }
+                    if (rowComplete)
+                    {
+                        break;
+                    }
+                }
+
+            }
+
+            return result;
+        }
+
+        public static int SummeraJamntDelbaraTal2(string[] indata)
+        {
+            int result = 0;
+
+            foreach (var row in indata)
+            {
+                var numbers = row.Split((char[])null, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    int currentNumber = numbers[i];
+
+                    int evenlyDivisiveNumber = numbers.Where((n, index) => i != index).FirstOrDefault(x => currentNumber % x == 0);
+
+                    if (evenlyDivisiveNumber != 0)
+                    {
+                        result += currentNumber / evenlyDivisiveNumber;
+                        break;
+                    }
+                }
+
+            }
+
+            return result;
+        }
     }
 
     public class Day2Tests
     {
         [Fact]
-        public void Test1()
+        public void Part1_Test()
         {
-            string data = "1    5 3" + Environment.NewLine + // 4
+            string data = "1    5 3" + Environment.NewLine +         // 4
                           "2    4       8" + Environment.NewLine +   // 6
-                          "1 2 3";      // 2
+                          "1 2 3";                                   // 2   -> sum == 12
 
             var rows = data.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
 
@@ -41,12 +103,51 @@ namespace AdventOfCode2017.Day2
         }
 
         [Fact]
-        public void Day2_Solution()
+        public void Day2_Part1_Solution()
         {
             var lines = File.ReadAllLines(@"Day2\puzzle_input.txt");
 
             Day2.SummeraSkillnader(lines).Should().Be(44216);
         }
 
+        [Fact]
+        public void Part2_Test_v1()
+        {
+            string data = "2 5 3 4" + Environment.NewLine +   // 2
+                          "3 4 7 16" + Environment.NewLine +  // 4
+                          "3 7 14";                           // 2    -> sum == 8
+
+            var rows = data.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            Day2.SummeraJamntDelbaraTal(rows).Should().Be(8);
+        }
+
+        [Fact]
+        public void Day2_Part2_Solution_v1()
+        {
+            var lines = File.ReadAllLines(@"Day2\puzzle_input.txt");
+
+            Day2.SummeraJamntDelbaraTal(lines).Should().Be(320);
+        }
+
+        [Fact]
+        public void Part2_Test_v2()
+        {
+            string data = "2 5 3 4" + Environment.NewLine +   // 2
+                          "3 4 7 16" + Environment.NewLine +  // 4
+                          "3 7 14";                           // 2    -> sum == 8
+             
+            var rows = data.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            Day2.SummeraJamntDelbaraTal2(rows).Should().Be(8);
+        }
+
+        [Fact]
+        public void Day2_Part2_Solution_v2()
+        {
+            var lines = File.ReadAllLines(@"Day2\puzzle_input.txt");
+
+            Day2.SummeraJamntDelbaraTal2(lines).Should().Be(320);
+        }
     }
 }
